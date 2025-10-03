90be108f189fa3f872c3333b96e53d797464210b
Samuel Fritsch <samuelfritsch@Samuels-MacBook-Air.local>
2025-10-03 11:15:58 -0500

    Added message 10/03

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const panda = document.querySelector('.panda');
 const wrapper = document.querySelector('.panda-wrapper');
 
 let url = 'https://api.sheety.co/503cd683d77f4feeb101a928a19c01b6/messageBank/sheet1';
-let dailyMessage = "Panda says oink oink"; // Default fallback
+let dailyMessage = "Panda goes moo"; // Default fallback
 
 async function loadDailyMessage() {
   try {
